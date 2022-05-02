c8a1203cc6fabb6913a21e7baa1e04ea74b504da
Alexander Snitko <asyncamisu@gmail.com>
2022-05-02 21:15:13 +0300

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Alexander Snitko on 2.05.22.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
